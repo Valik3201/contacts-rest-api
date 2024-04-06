e54b9b1d56a84b8a0db4377b4f085e1b82d451e1
Valik <valik3201@gmail.com>
2024-04-06 23:38:46 +0200

    refactor(router): update API router for database integration

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,14 +6,18 @@ const {
   listContacts,
   removeContact,
   updateContact,
-} = require("../../models/contacts");
+} = require("../../controllers/contactController");
 
-const { contactSchema, contactIdSchema } = require("./validation");
+const {
+  contactSchemaForCreate,
+  contactSchemaForUpdate,
+  contactIdSchema,
+} = require("./validation");
 
 router.get("/", async (req, res, next) => {
   try {
     const contacts = await listContacts();
-    res.json(contacts);
+    res.status(200).json(contacts);
   } catch (error) {
     next(error);
   }
@@ -33,7 +37,7 @@ router.get("/:contactId", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { error } = contactSchema.validate(req.body);
+    const { error } = contactSchemaForCreate.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
@@ -69,7 +73,7 @@ router.put("/:contactId", async (req, res, next) => {
       return res.status(400).json({ message: idError.details[0].message });
     }
 
-    const { error: bodyError } = contactSchema.validate(req.body);
+    const { error: bodyError } = contactSchemaForUpdate.validate(req.body);
     if (bodyError) {
       return res.status(400).json({ message: bodyError.details[0].message });
     }
