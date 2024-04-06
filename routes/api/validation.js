e555099c06268b64343a4c50d809ee7b58c67f58
Valik <valik3201@gmail.com>
2024-04-06 23:38:57 +0200

    refactor(validation): update validation

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const contactSchema = Joi.object({
+const contactSchemaForCreate = Joi.object({
   name: Joi.string().trim().min(1).max(50).required(),
   email: Joi.string().email().required(),
   phone: Joi.string()
@@ -10,10 +10,19 @@ const contactSchema = Joi.object({
     .required(),
 });
 
-const contactIdSchema = Joi.string()
-  .guid({
-    version: ["uuidv4"],
-  })
-  .required();
+const contactSchemaForUpdate = Joi.object({
+  name: Joi.string().trim().min(1).max(50),
+  email: Joi.string().email(),
+  phone: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .min(9)
+    .max(15),
+});
 
-module.exports = { contactSchema, contactIdSchema };
+const contactIdSchema = Joi.string().required();
+
+module.exports = {
+  contactSchemaForCreate,
+  contactSchemaForUpdate,
+  contactIdSchema,
+};
