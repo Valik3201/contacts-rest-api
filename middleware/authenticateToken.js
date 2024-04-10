bdafb3134b67f8fce7f9c6e1281ab88a567cfb32
Valik <valik3201@gmail.com>
2024-04-10 19:26:52 +0200

    refactor: remove unnecessary comments

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -3,26 +3,21 @@ const User = require("../models/user");
 
 const authenticateToken = async (req, res, next) => {
   try {
-    // Получаем токен из заголовков Authorization
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Not authorized" });
     }
 
-    // Проверяем токен на валидность
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Получаем id пользователя из токена
     const userId = decoded.userId;
 
-    // Находим пользователя в базе данных
     const user = await User.findById(userId);
     if (!user || user.token !== token) {
       return res.status(401).json({ message: "Not authorized" });
     }
 
-    // Записываем данные пользователя в req.user и вызываем метод next()
     req.user = user;
     next();
   } catch (error) {
